b3222bef21a2a3b0bf82bcd5e50fb92cdf6ef024
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-03 14:56:33 +0200

    validate signature for webhooks only

--- packages/server-extension/src/routes/index.js
+++ packages/server-extension/src/routes/index.js
@@ -16,11 +16,10 @@ const oneWeek = 604800
 const oneDay = 86400
 
 router.use(loggerMiddleware)
-router.use(validateWebhookMiddleware)
 router.use(uaParser)
 router.use(occClient, createClient)
 
-router.use('/v1/payments', payments)
+router.use('/v1/payments', validateWebhookMiddleware, payments)
 router.use('/v1/paymentMethods', cache(oneWeek), paymentMethods)
 router.use('/v1/originKeys', cache(oneDay), originKeys)
 router.use('/v1/clearCache', clearCache)
